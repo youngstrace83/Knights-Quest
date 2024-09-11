c39516b286b8606ec0fe698360d5455984e8200c
Jordan Stracener <jordan.stracener83@gmail.com>
2024-09-11 16:25:24 -0400

    Added keys to game as a pick up item for player

--- quest.py
+++ quest.py
@@ -30,13 +30,17 @@ def grid_coords(actor):
     return (round(actor.x / GRID_SIZE), round(actor.y / GRID_SIZE)) # Determines the position of an actor on the grid
 
 def setup_game():
-    global player # Defines player as a global variable
+    global player, keys_to_collect # Defines player and keys_to_collect as a global variables
     player = Actor("player", anchor=("left", "top")) # Creates a new Actor object and sets its anchor position
+    keys_to_collect = [] # Sets keys_to_collect to an empty list initially
     for y in range(GRID_HEIGHT): # Loops over each grid position
         for x in range(GRID_WIDTH): 
             square = MAP[y][x] # Extracts the character from the map representing this grid position
             if square == "P": # Checks if this grid position is the player
                 player.pos = screen_coords(x, y) # Sets the position of player to the screen coordinates of the grid position
+            elif square == "K": # Creates a key if the square is K
+                key = Actor("key", anchor=("left", "top"), pos=screen_coords(x, y)) # Creates the key actor with an image, anchor, and position
+                keys_to_collect.append(key) # Adds this actor to the list of keys created above
 
 def draw_background():
     for y in range(GRID_HEIGHT): # Loops over each grid row
@@ -54,6 +58,8 @@ def draw_scenery():
 
 def draw_actors():
     player.draw() # Drows the player actor onscreen at it's current position
+    for key in keys_to_collect: # Draws all the actors in the list keys_to_collect
+        key.draw()
 
 # The draw handler function is called automatically from the game loop
 def draw():
@@ -71,7 +77,7 @@ def on_key_down(key): # Reacts when the user presses down on a key
     elif key == keys.DOWN:
         move_player(0, 1) # Player moves down by one grid Square
 
-def move_player(dx, dy)
+def move_player(dx, dy):
     (x, y) = grid_coords(player) # Gets the current grid position of player
     x += dx # Adds the x axis distance to x
     y += dy # Adds the y axis distance to y
@@ -79,7 +85,14 @@ def move_player(dx, dy)
     if square == "W":
         return # Stops the execution of the move_player() function, if the player touches the wall
     elif square == "D":
-        return # Returns immediately if it is a door
+        if len(keys_to_collect) > 0:
+            return # Returns immediately if list is not empty
+    for key in keys_to_collect: # Loops over each of the key actors in the list
+        (key_x, key_y) = grid_coords(key) # Gets the grid position of a key actor
+        if x == key_x and y == key_y: # Checks if the new player position matches the key position
+            keys_to_collect.remove(key) # Removes this key from the list if player position matches key position
+            break # Breaks out of the for loop, as each square can only contain one key
+
     player.pos = screen_coords(x, y) # Updates position of player to the new coordinates
 
 setup_game()
